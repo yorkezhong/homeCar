821d575bf3254c81d1ac6ea9b7ffc16718f91b61
zhongyoufeng <1397715330@qq.com>
2019-06-19 20:06:54 +0800

    add codeing

--- components/evaluate/evaluate.js
+++ components/evaluate/evaluate.js
@@ -11,13 +11,19 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    tabCur: 0,
+    starnum:4
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    tabselect(e) {
+      this.setData({
+        tabCur: e.target.dataset.id
+      })
+    },
+    
   }
 })
